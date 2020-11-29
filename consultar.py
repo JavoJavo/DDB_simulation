bcb1ee71dfc8148bb9fce917fda5aab6cb06e9b5
luasikirf <60940567+luasikirf@users.noreply.github.com>
2020-11-29 13:25:11 -0600

    funcion consulta por cualquier atributo

--- consultar.py
+++ consultar.py
@@ -1,118 +1,103 @@
-import mariadb 
-#pip3 install prettytable
 from prettytable import PrettyTable
-
-## SOLO SIRVE PARA RELACIONES UNO A UNO,(NO SIRVE PARA RELACIONES UNO A MUCHOS O MUCHOS A MUCHOS)
-
-
-def dame_sucursales():
-    conn = mariadb.connect(
-        user = "javo", 
-        password = "b", 
-        host = "127.0.0.1", 
-        database = 'sucursales'
-        ) 
-    
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM nombres")
-    nombres = []
-    for suc in cur:
-        (n,) = suc
-        #n = n[0]
-        nombres.append(n)
-    return nombres
-
-
+import json
+import mysql.connector
+PATH='/home/saul/Escritorio/'
+if __name__ == "__main__":
+    with open(PATH+'config.json') as json_file:
+        config = json.load(json_file)
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
     
 def graficar(columnas, data):
     t = PrettyTable(columnas)
     for item in data:
         t.add_row(item)
     print(t)
-    '''
-    row_format ="{:>15}" * (len(columnas) + 1)
-    print(row_format.format("", *columnas))
-    for team, row in zip(columnas, data):
-        print(row_format.format(team, *row))
-    '''
-    
 
-def consultar_cliente(bd,atributo, value):
+def dame_sucursales():
+    nombres = []
+    cursor.execute("use sucursales")
+    cursor.execute("SELECT * FROM nombres")
+    for suc in cursor:
+        (n,) = suc
+        nombres.append(n)
+    return nombres
     
-    
-    conn = mariadb.connect(
-        user = "javo", 
-        password = "b", 
-        host = "127.0.0.1", 
-        database = bd
-        ) 
-    
-    IdDir = 0 # para que no truene en caso de consulta vacia
-    
-    if atributo == 'Nombre' or atributo == 'ApellidoPaterno' or atributo == 'ApellidoMaterno' or atributo == 'RFC' or atributo == 'IdCli':
-        data = []
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Clientes WHERE {} = '{}'".format(atributo, value))
-        for tupla in cur:
-            (IdCi,n,aM,aP,RFC,IdDir) = tupla
-            data.append([IdCi,n,aM,aP,RFC,IdDir])
-            #print(IdCi,n,aM,aP,RFC,IdDir)#, end = '')
-        
-        #i = 0
-        for i in range(len(data)):
-            cur.execute("SELECT * FROM Direcciones WHERE IdDir = '{}'".format(data[i][5]))#IdDir))
-            for tupla in cur:
-                (IdDir,ca,num,col,loc,est,CP) = tupla
-                data[i] = data[i] + [ca,num,col,loc,est,CP]
-                #print(IdDir,ca,num,col,loc,est,CP)
-                #i += 1
-        
-        conn.close()
-        return(data)
-    
-    
-    else:
-        #print('paso')
-        data = []
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Direcciones WHERE {} = '{}'".format(atributo, value))
-        for tupla in cur:
-            (IdDir,ca,num,col,loc,est,CP) = tupla
-            data.append([IdDir,ca,num,col,loc,est,CP])
-            #print(IdDir,ca,num,col,loc,est,CP)
-        
-        #i = 0
-        #while i < len(data):
-        for i in range(len(data)):
-            cur.execute("SELECT * FROM Clientes WHERE IdDir = '{}'".format(data[i][0]))
-            tuplas = []
-            for tupla in cur:
-                (IdCi,n,aM,aP,RFC,IdDir) = tupla
-                data[i] = [IdCi,n,aM,aP,RFC] + data[i]
-                #print(IdCi,n,aM,aP,RFC,IdDir)
-                #i = i +1
-                #print(i)
-        conn.close()
-        return (data)
+def Consultar_cliente():
+    return(None)
 
-
-def Consultar_clientes(atributo,valor):
+def Consultar_clientes():
+    return(None)
+def Consultar_direccion_id():
+    sucursales=dame_sucursales()
+    #print(sucursales)
+    idDir=int(input("dame el id de la direccion: "))
+    registros=[]
+    for sucursal in sucursales:
+        #print(sucursal)
+        query=("use {}").format(sucursal)
+        cursor.execute(query)
+        query=("select * from Direcciones where idDir = {}").format(idDir)
+        cursor.execute(query)
+        for (idDir,calle,numero,colonia,localidad,estado,CP) in cursor:
+            registro1=[calle,numero,colonia,localidad,estado,CP]
+        query=("select * from Clientes where idDir = {}").format(idDir)
+        cursor.execute(query)
+        for (IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC,idDir) in cursor:
+            registro2=[IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC]
+        rf=(IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC,idDir,calle,numero,colonia,localidad,estado,CP)
+        registros.append(rf)
+    #print(registros)
+    return(registros)
+        
+        
+def Consultar_direccion_noID(calle,numero,colonia,localidad,estado,cp):
+    sucursales=dame_sucursales()
+    registros=[]
+    dire=None
+    '''if type(numero) != int and numero != ' ':
+        numero=int(numero)'''
+    for sucursal in sucursales:
+        query=("use {}").format(sucursal)
+        cursor.execute(query)
+        query=("select * from Direcciones where calle like '%{}%' and numero like '{}' and colonia like '%{}%' and localidad like '%{}%' and estado like '%{}%' and CP like '%{}%' ").format(calle,numero,colonia,localidad,estado,cp)
+        cursor.execute(query)
+        for (idDir,calle,numero,colonia,localidad,estado,CP) in cursor:
+            dire=idDir
+        query=("select * from Clientes where idDir = '{}'").format(dire)
+        cursor.execute(query)
+        for (IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC,idDir) in cursor:
+            registro2=[IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC]
+            rf=(IdCli,Nombre,ApellidoPaterno,ApellidoMaterno,RFC,idDir,calle,numero,colonia,localidad,estado,CP)
+            registros.append(rf)
+    print(registros)
+    return(registros)
+    
+def Consultar_direciones():
     columnas = ['IdCli','Nombre','Apellido1','Apellido2','RFC','IdDir', 'Calle', 'Numero','Colonia','Localidad','Estado','CP']
-    sucs = dame_sucursales()
-    data = []
+    print("sabe la id de la direccion?")
+    print("1.- Si")
+    print("2.- No")
+    a=int(input())
+    if a == 1:
+        data=Consultar_direccion_id()
+        graficar(columnas, data)
+    if a == 2:
+        calle=input("ingrese la calle, si no la sabe de enter")
+        numero=input("ingrese el numero, si no lo sabe de enter")
+        colonia=input("ingrese la colonia, si no la sabe de enter")
+        localidad=input("ingrese la localidad, si no la sabe de enter")
+        estado=input("ingrese el estado, si no lo sabe de enter")
+        cp=input(" ingrese el CP, si no lo sabe de enter")
+        data=Consultar_direccion_noID(calle,numero,colonia,localidad,estado,cp)
+        graficar(columnas, data)
+
     
-    for suc in sucs:
-        registros = consultar_cliente(suc,atributo,valor)
-        data = data + registros
-        #print(data)
-    #for d in data:
-        #print(d)
-        #None
-    graficar(columnas, data)
+Consultar_direciones()
     
-'''
-atributo = input('Dame atributo:')
-valor = input('Dame valor: ')
-Consultar_clientes(atributo, valor)
-'''
-Consultar_clientes('Localidad','Pátzcuaro')
+    
+    
+    
+    
+    
+    
